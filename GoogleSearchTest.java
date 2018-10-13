690cf8005f8f5e1a111c2e1b669225696789c08a
kalpal <kalpal@users.noreply.github.com>
2018-10-13 17:44:40 -0400

    Fixed the test

---
+++ GoogleSearchTest.java
@@ -0,0 +1,30 @@
+package uiAutomation.tests.search;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import uiAutomation.BaseTest;
+import uiAutomation.pages.google.MainPage;
+import uiAutomation.pages.google.ResultPage;
+
+
+public class GoogleSearchTest extends BaseTest {
+
+    @Test
+    public void test001() {
+        MainPage mainPage = new MainPage(driver);
+        ResultPage resultPage = new ResultPage(driver);
+
+        String queryString = "query";
+
+        mainPage.open();
+        mainPage.typeRequestInSearchInput(queryString);
+        mainPage.submitSearch();
+
+        resultPage.printOutAmountOfResult();
+
+        String actualPageTitle = resultPage.getPageTitle();
+        String expectedPageTitle = "query - Google Search";
+
+        Assert.assertEquals(actualPageTitle, expectedPageTitle);
+    }
+}
